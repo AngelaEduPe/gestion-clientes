f777c0b80e2098c9df0c386220f5ab21d55861fe
Yrahi Quispe Reyna <yira1231@hotmail.com>
2025-03-14 17:00:12 -0500

    CREATE Oportunidad

---
+++ src/main/java/services/OportunidadService.java
@@ -0,0 +1,8 @@
+package services;
+
+import java.util.List;
+import models.Oportunidad;
+
+public interface OportunidadService {
+    List<Oportunidad> listarOportunidades();
+}
---
+++ src/main/java/services/impl/OportunidadServiceImpl.java
@@ -0,0 +1,23 @@
+package services.impl;
+
+import java.util.List;
+
+import config.MyBatisUtil;
+import dao.OportunidadMapper;
+import models.Oportunidad;
+import services.OportunidadService;
+
+public class OportunidadServiceImpl implements OportunidadService {
+
+    @Override
+    public List<Oportunidad> listarOportunidades() {
+        try {
+            SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+            OportunidadMapper oportunidadMapper = session.getMapper(OportunidadMapper.class);
+            return oportunidadMapper.listarOportunidades();
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
